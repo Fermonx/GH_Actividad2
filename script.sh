137ff37ab3e887c902bd389254b20e11b928679d
FernanMonc <fernandomdsatoutlook.com>
2018-03-19 15:36:19 +0100

    Reinstalled vagrant with a new bash script, tested that the webiste and the server works, location button now disappears from the navbar on mobile

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/env bash
 
+
+#!/usr/bin/env bash
 apt-get update
 apt-get install -y apache2
-chmod o+rwX /var/www/html
+if ! [ -L /var/www/html/ ]; then
+sudo rm -rf /var/www/html/
+sudo ln -fs /vagrant /var/www/html
+fi
